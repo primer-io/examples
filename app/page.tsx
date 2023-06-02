52d0bd4d0bf2474f8d092593ef3c1e5baefc522e
Jedrzej Lewandowski <jedrzej@primer.io>
2023-06-02 06:32:33 +0200

    [FEAT] Add More Text (#20)
    [TICKET]
    
    # What / How / Why
    
    Describe ... (Features / UI / logic changes)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 import styles from './page.module.scss'
-import {BrandIcon, H1} from "@primer-io/goat";
+import { BrandIcon, H1, P } from "@primer-io/goat";
 export default function Home() {
   return (
     <main className={styles.main}>
         <H1>Primer Playground Repo</H1>
+        <P>
+          This is a playground repo for testing out Primer components and styles.
+        </P>
         <BrandIcon src='https://goat-assets.production.core.primer.io/brand/icon/primer.svg' size='large' className={styles.logo} />
     </main>
   )

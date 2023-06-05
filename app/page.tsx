a5eb7de8e4a92ff12e989c98e2731aa136f484a0
Jedrzej (Andrew) Lewandowski <jedrzej@primer.io>
2023-06-05 11:09:24 +0200

    Add Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 import styles from './page.module.scss'
-import { BrandIcon, H1, P } from "@primer-io/goat";
+import { BrandIcon, H1, Link, P } from "@primer-io/goat";
 export default function Home() {
   return (
     <main className={styles.main}>
         <H1>Primer Playground Repo</H1>
         <P>
           This is a playground repo for testing out Primer components and styles.
+        </P>
+        <P>
+          Check out the <Link href="https://goat-primer-io.vercel.app/">Goat docs</Link> for more info.
         </P>
         <BrandIcon src='https://goat-assets.production.core.primer.io/brand/icon/primer.svg' size='large' className={styles.logo} />
     </main>

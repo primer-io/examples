9cd153895fcb4078e9ffe972bfb93e95cbc90913
Jedrzej Lewandowski <jedrzej@primer.io>
2025-11-07 15:13:16 +0100

    Fix links and add change logs.

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://composable-checkout.vercel.app',
+  url: 'https://web-components.primer.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
